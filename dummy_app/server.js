8a51693ba538c37ee1f448d46f5738e6246d519b
notshahzad <shahzadakhtar182005@gmail.com>
2021-02-05 16:14:19 +0530

    node basic text commit app

---
+++ dummy_app/server.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var cdir = `${__dirname}/`;
+var shit_to_print = {};
+var usermsg;
+app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine','ejs');
+app.use(express.static(__dirname));
+app.get('/:useradd',(req,res)=>{
+    if (req.params.useradd!='favicon.ico' && !(req.params.useradd in shit_to_print)){
+        usermsg = req.params.useradd;
+        shit_to_print[usermsg] = [];
+    };
+    console.log(shit_to_print);
+    res.render('index',{'shittoprint' : shit_to_print[usermsg]});
+});
+app.post('/msgadd',(req,res)=>{
+    console.log(req.body.msgbox01)
+    shit_to_print[usermsg].push(req.body.msgbox01)
+    res.render('index',{'shittoprint' : shit_to_print[usermsg]})
+});
+app.listen(8080)
