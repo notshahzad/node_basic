0b061cae3846f70e0e3b6835d3319c92aa129601
notshahzad <shahzadakhtar182005@gmail.com>
2021-02-09 20:35:02 +0530

    'smol_change'

--- dummy_app/server.js
+++ dummy_app/server.js
@@ -7,6 +7,9 @@ var usermsg;
 app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine','ejs');
 app.use(express.static(__dirname));
+app.get('/',(req,res)=>{
+    res.end('type your name in url.')
+});
 app.get('/:useradd',(req,res)=>{
     if (req.params.useradd!='favicon.ico' && !(req.params.useradd in shit_to_print)){
         usermsg = req.params.useradd;
@@ -16,8 +19,11 @@ app.get('/:useradd',(req,res)=>{
     res.render('index',{'shittoprint' : shit_to_print[usermsg]});
 });
 app.post('/msgadd',(req,res)=>{
-    console.log(req.body.msgbox01)
-    shit_to_print[usermsg].push(req.body.msgbox01)
-    res.render('index',{'shittoprint' : shit_to_print[usermsg]})
+    console.log(req.body.msgbox01);
+    shit_to_print[usermsg].push(req.body.msgbox01);
+    res.redirect(usermsg);
+});
+app.get('showmemsg',(req,res)=>{
+    res.send(shit_to_print);
 });
 app.listen(8080)
